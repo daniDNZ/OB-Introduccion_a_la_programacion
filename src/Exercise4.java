90c2ef068ec587cccb112c6721bd4ff0ef7aba54
danisanz <danielsanzelguer@gmail.com>
2022-06-03 11:51:00 +0200

    Statements

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,3 +1,14 @@
+/*
+* En este ejercicio practicarás las estructuras de control, para ello deberás crear:
+* Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+* Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+* Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+* Incrementar el valor de la variable en uno cada vez que se ejecute.
+* Mostrarlo por pantalla cada vez que se ejecute.
+* Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+* Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+* Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+*/
 public class Exercise4 {
     public static void main() {
 
@@ -50,21 +61,11 @@ public class Exercise4 {
 
     public static void seasonSwitch(String season) {
         switch (season.toLowerCase()) {
-            case "spring":
-                System.out.println("Spring");
-                break;
-            case "summer":
-                System.out.println("Summer");
-                break;
-            case "autumn":
-                System.out.println("Autumn");
-                break;
-            case "winter":
-                System.out.println("Winter");
-                break;
-            default:
-                System.out.println(season+" is not a season");
-                break;
-        };
+            case "spring" -> System.out.println("Spring");
+            case "summer" -> System.out.println("Summer");
+            case "autumn" -> System.out.println("Autumn");
+            case "winter" -> System.out.println("Winter");
+            default -> System.out.println(season + " is not a season");
+        }
     }
 }
