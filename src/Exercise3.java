679c80e0eec95245859fab0afdf6e096dfeeafcc
danisanz <danielsanzelguer@gmail.com>
2022-06-03 11:19:18 +0200

    Exercise3

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Exercise3 {
+public class Exercise3 {
+    public static void main() {
+        System.out.println("++++++++++ EXERCISE 3 ++++++++++");
+
+        toAddNumbers(1, 2, 3);
+
+        Car vw = new Car();
+        vw.addDoor();
+        System.out.println("Doors: "+vw.doors);
+
+    }
+
+    public static void toAddNumbers(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+
+}
+
+class Car {
+    public int doors;
+
+    public Car() {
+        doors = 1;
+    }
+
+    public void addDoor() {
+        this.doors++;
+    }
+
 }
