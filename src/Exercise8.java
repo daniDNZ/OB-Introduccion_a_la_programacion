04fc797bfe7c1029544e127a22ea0419d5eb8445
danisanz <danielsanzelguer@gmail.com>
2022-06-03 11:36:42 +0200

    Exercise8

---
+++ src/Exercise8.java
@@ -0,0 +1,58 @@
+/*
+* Para practicar la encapsulación:
+* Crear clase Persona.
+* Crear variables las privadas edad, nombre y telefono de la clase Persona.
+* Crear gets y sets de cada propiedad.
+* Crear un objeto persona en el main.
+* Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
+ */
+
+public class Exercise8 {
+    public static void main() {
+        System.out.println("++++++++++ EXERCISE 8 ++++++++++");
+
+        Person juan = new Person();
+        juan.setName("Juan");
+        juan.setAge(34);
+        juan.setPhone(645839456);
+
+        System.out.println("Me llamo "+juan.getName()+" y tengo "+juan.getAge()+" años.");
+        System.out.println("Mi teléfono es: "+juan.getPhone()+".");
+    }
+}
+
+class Person {
+    private int age;
+    private String name;
+    private int phone;
+
+    public Person(){
+        age = 0;
+        name = "";
+        phone = 0;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPhone(int newPhone) {
+        this.phone = newPhone;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Exercise3.main();
         Exercise4.main();
+        Exercise8.main();
     }
 }
